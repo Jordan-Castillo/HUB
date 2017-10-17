868ecec67cc26f2cfc3622e08ea11e3bc9a1c9f6
Jordan-Castillo <jtcastil@calpoly.edu>
2017-10-17 16:16:00 -0700

    Initial Commit

---
+++ src/example/start/Start.java
@@ -0,0 +1,11 @@
+package example.start;
+import java.awt.*;
+
+/**
+ * Created by jtcas on 9/11/2017.
+ */
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
---
+++ src/mygui/myGuiProgram.java
@@ -0,0 +1,22 @@
+package mygui;
+import java.awt.Frame;
+
+/**
+ * Created by jtcas on 9/11/2017.
+ */
+public class myGuiProgram extends Frame {
+
+    // Private Variables
+    int test;
+    // Methods
+    public myGuiProgram() {
+        test = 0;
+    }
+
+    public static void main(String[] args) {
+        myGuiProgram gui;
+        gui = new myGuiProgram();
+        //making progress....
+    }
+
+}
